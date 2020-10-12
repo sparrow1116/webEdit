558e56bb3744b8db2e8ecd3d455ef783f35ed4d1
sparrow1116 <yingjiez1@126.com>
2020-10-12 22:02:48 +0800

    zyj commit for first version

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 
-const isServer = false;
+const isServer = true;
 
 const config = {
-    baseUrl: isServer ? 'http://47.103.124.76:3000':'http://192.168.10.7:3000',//'http://192.168.10.7:3000'
+    baseUrl: isServer ? 'https://www.yangmaoba.club':'http://192.168.10.7:3000',//'http://192.168.10.7:3000'
     tmpImgBase: 'https://wollpic.oss-cn-shanghai.aliyuncs.com'//:'http://192.168.1.6:7777'//'http://172.171.2.211:7777'//'http://192.168.10.7:7777/'
 };
 export default config
